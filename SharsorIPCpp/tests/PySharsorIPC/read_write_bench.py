5af526399144a5678ea808ff06037950f62ca8b1
andrea patrizi <andreapatrizi1b6e6@gmail.com>
2023-11-01 19:22:49 +0100

    starting to add typed performance tests

--- SharsorIPCpp/tests/PySharsorIPC/read_write_bench.py
+++ SharsorIPCpp/tests/PySharsorIPC/read_write_bench.py
@@ -23,12 +23,13 @@ class TestPerfBenchBase(unittest.TestCase):
         self.cols = N_COLS
         self.iterations = N_ITERATIONS
 
-        self.layout = None
-        self.dtype = None
+        self.is_release = isRelease()
 
         self.server = ServerFactory(self.rows,
                                     self.cols,
-                                    basename="PySharsor",
+                                    basename="PySharsor" + \
+                                    str(self.layout)  +
+                                    str(self.data_type),
                                     namespace=namespace,
                                     verbose=True,
                                     vlevel=VLevel.V3,
@@ -40,12 +41,12 @@ class TestPerfBenchBase(unittest.TestCase):
 
             self.order = 'C'
 
-        if self.layout == RowMajor:
+        if self.layout == ColMajor:
 
             self.order = 'F'
 
-        self.tensor_copy = np.zeros((server.getNRows(), server.getNCols()),
-                                dtype=toNumpyDType(server.getScalarType()), # dtype has to match
+        self.tensor_copy = np.zeros((self.server.getNRows(), self.server.getNCols()),
+                                dtype=toNumpyDType(self.server.getScalarType()), # dtype has to match
                                 order=self.order)
 
     def tearDown(self):
@@ -54,81 +55,36 @@ class TestPerfBenchBase(unittest.TestCase):
 
     def readAndWrite(self):
 
-        check_comp_type(journal);
+        a = 1
 
-        double READ_T_MAX_THRESH = Thresholds<ScalarType, layout>::READ_T_MAX_THRESH;
-        double WRITE_T_MAX_THRESH = Thresholds<ScalarType, layout>::WRITE_T_MAX_THRESH;
-        double READ_T_AVRG_THRESH = Thresholds<ScalarType, layout>::READ_T_AVRG_THRESH;
-        double WRITE_T_AVRG_THRESH = Thresholds<ScalarType, layout>::WRITE_T_AVRG_THRESH;
+class TestPerfBenchBoolColMaj(TestPerfBenchBase):
 
-        std::vector<double> readTimes;
-        std::vector<double> readTimesView;
-        std::vector<double> writeTimes;
+    data_type = dtype.Bool
+    layout = ColMajor
 
-        journal.log("PerfTest", "\nBenchmarking performance...\n",
-                    Journal::LogType::STAT);
+    def test_write_read(self):
 
-        Tensor<ScalarType, layout> tensor_other = Tensor<ScalarType, layout>::Zero(3 * this->rows,
-                                           2 * this->cols); // tensor of which a view is created
+        if self.is_release:
 
-        for (int i = 0; i < this->iterations; ++i) {
+            message = "SharsorIPCpp was compiled in Release mode. " + \
+                "This is good and will ensure meaningful benchmarking results.\n"
 
-            Tensor<ScalarType, layout> myData(this->rows, this->cols);
-            myData.setRandom(); // we generate a random tensor of the right size
+            Journal.log(self.__class__.__name__,
+                        "test_write_read",
+                        message,
+                        LogType.STAT)
 
-            // we measure the time to write it on the memory
-            auto startWrite = std::chrono::high_resolution_clock::now();
-            this->server_ptr->writeTensor(myData);
-            auto endWrite = std::chrono::high_resolution_clock::now();
-            double writeTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endWrite - startWrite).count();
-            writeTimes.push_back(writeTime);
+        if not self.is_release:
 
-            // we measure the time to read a copy of the tensor
-            auto startRead = std::chrono::high_resolution_clock::now();
-            this->server_ptr->readTensor(this->tensor_copy, 0, 0);
-            auto endRead = std::chrono::high_resolution_clock::now();
-            double readTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endRead - startRead).count();
-            readTimes.push_back(readTime);
+            message = "SharsorIPCpp was not compiled in Release mode. " + \
+                "Please compile it in Release mode to get meaningful results.\n"
 
-        }
+            Journal.log(self.__class__.__name__,
+                        "test_write_read",
+                        message,
+                        LogType.WARN)
 
-        journal.log("PerfTest", "\nrunning post-processing steps...\n",
-                    Journal::LogType::STAT);
-
-        // some post-processing
-        double averageReadTime = 0;
-        double averageWriteTime = 0;
-        double maxReadTime = std::numeric_limits<double>::min();
-        double maxWriteTime = std::numeric_limits<double>::min();
-
-        for (int i = 0; i < this->iterations; ++i) {
-            averageReadTime += readTimes[i];
-            averageWriteTime += writeTimes[i];
-
-            if (readTimes[i] > maxReadTime) {
-                maxReadTime = readTimes[i];
-            }
-
-            if (writeTimes[i] > maxWriteTime) {
-                maxWriteTime = writeTimes[i];
-            }
-        }
-
-        averageReadTime /= this->iterations;
-        averageWriteTime /= this->iterations;
-
-        std::cout << "Number of performed iterations: " << this->iterations << std::endl;
-        std::cout << "Average Read (with copy) Time: " << averageReadTime << " ns" << std::endl;
-        std::cout << "Average Write Time: " << averageWriteTime << " ns" << std::endl;
-        std::cout << "Maximum Read (with copy) Time: " << maxReadTime << " ns" << std::endl;
-        std::cout << "Maximum Write Time: " << maxWriteTime << " ns\n" << std::endl;
-
-        // Checking if perf. req. were met
-
-        // reading (avrg)
-        ASSERT_LT(averageReadTime, READ_T_AVRG_THRESH);
-        ASSERT_LT(averageWriteTime, WRITE_T_AVRG_THRESH);
-
+        self.readAndWrite()
 
 if __name__ == "__main__":
 
