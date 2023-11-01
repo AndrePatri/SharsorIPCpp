287ca2eb0725b59bc145f12b9c52eb62ad9387ee
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2023-11-01 17:56:19 +0100

    minor change

--- SharsorIPCpp/bindings/PySharsorIPC/PySharsorIPC.cpp
+++ SharsorIPCpp/bindings/PySharsorIPC/PySharsorIPC.cpp
@@ -4,17 +4,14 @@
 #include <PySharsorIPC/PyServer.hpp>
 #include <PySharsorIPC/PyClient.hpp>
 
-
-namespace py = pybind11;
-
 using namespace SharsorIPCpp;
 using namespace PySharsorIPC;
 
 PYBIND11_MODULE(PySharsorIPC, m) {
 
-    m.doc() = "pybind11 StringTensor bindings";
+    m.doc() = "pybind11 SharsorIPCpp bindings";
 
-    py::enum_<DType>(m, "dtype")
+    pybind11::enum_<DType>(m, "dtype")
         .value("Bool", DType::Bool)
         .value("Int", DType::Int)
         .value("Float", DType::Float)
@@ -23,7 +20,7 @@ PYBIND11_MODULE(PySharsorIPC, m) {
     m.attr("RowMajor") = RowMajor;
     m.attr("ColMajor") = ColMajor;
 
-    py::enum_<VLevel>(m, "VLevel")
+    pybind11::enum_<VLevel>(m, "VLevel")
         .value("V0", Journal::VLevel::V0)
         .value("V1", Journal::VLevel::V1)
         .value("V2", Journal::VLevel::V2)
@@ -33,10 +30,10 @@ PYBIND11_MODULE(PySharsorIPC, m) {
     // In your Pybind11 bindings:
     m.def("toNumpyDType", [](DType dtype) {
         switch(dtype) {
-            case DType::Bool: return py::dtype::of<bool>();
-            case DType::Int: return py::dtype::of<int>();
-            case DType::Float: return py::dtype::of<float>();
-            case DType::Double: return py::dtype::of<double>();
+            case DType::Bool: return pybind11::dtype::of<bool>();
+            case DType::Int: return pybind11::dtype::of<int>();
+            case DType::Float: return pybind11::dtype::of<float>();
+            case DType::Double: return pybind11::dtype::of<double>();
 
             default: throw std::runtime_error("Unsupported DType conversion!");
         }
