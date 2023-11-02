5101e60d7e0a9192dc729d53b8dbb21ec54152eb
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-11-02 14:59:15 +0100

    added consistency checks for view API

--- SharsorIPCpp/tests/consistency_checks_clnt.cpp
+++ SharsorIPCpp/tests/consistency_checks_clnt.cpp
@@ -39,6 +39,7 @@ using MyTypes = ::testing::Types<
     TypeWithLayout<double, RowMajor>
 >;
 
+// normal tensor API
 template <typename P>
 class ConsistencyChecks : public ::testing::Test {
 protected:
@@ -70,6 +71,159 @@ protected:
         client_terminate_ptr->attach();
         client_flag_ptr->attach();
 
+
+        getDataFromServer();
+
+        data_read = Tensor<ScalarType, layout>::Zero(rows, cols);;
+
+
+    }
+
+    void Run() {
+
+        while (!terminate(0, 0)) { // exit if received a signal from server
+
+            while (!client_terminate_ptr->readTensor(terminate)) {
+
+                // try again
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+
+            }
+
+            while (!client_flag_ptr->readTensor(flag)) {
+
+                // try again
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+
+            }
+
+            while (!flag(0,0)){
+
+                while (!client_terminate_ptr->readTensor(terminate)) {
+
+                    // try again
+                    std::this_thread::sleep_for(std::chrono::microseconds(1));
+
+                }
+                if (terminate(0, 0)) {
+
+                    return;
+                }
+
+                client_flag_ptr->readTensor(flag); // reads flag from client
+
+                std::this_thread::sleep_for(std::chrono::microseconds(1)); // wait
+
+            }
+
+            // server has written new ping memory, let's read it
+
+            while (!client_ping_ptr->readTensor(data_read, 0, 0)) {
+
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+
+            // and write it on the pong memory
+
+            while (!client_pong_ptr->writeTensor(data_read, 0, 0)) {
+
+            }
+
+            // signaling the server that the read/write operation was completed
+            flag(0, 0) = false;
+            while (!client_flag_ptr->writeTensor(flag)) {
+
+                // try again
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+
+            }
+
+        }
+
+    }
+
+    void getDataFromServer() {
+
+        rows = client_ping_ptr->getNRows();
+        cols = client_ping_ptr->getNCols();
+
+        // reads initializations from Server
+
+        terminate = Tensor<bool>::Zero(client_terminate_ptr->getNRows(),
+                                       client_terminate_ptr->getNCols());
+        flag = Tensor<bool>::Zero(client_flag_ptr->getNRows(),
+                                  client_flag_ptr->getNCols());
+
+        client_terminate_ptr->readTensor(terminate);
+        client_flag_ptr->readTensor(flag);
+
+
+    }
+
+    void SetUp() override {
+
+        // Initialization code (if needed)
+    }
+
+    void TearDown() override {
+
+        // close all clients
+        client_ping_ptr->close();
+        client_pong_ptr->close();
+        client_terminate_ptr->close();
+        client_flag_ptr->close();
+
+    }
+
+    int rows;
+    int cols;
+
+    typename Client<ScalarType, layout>::UniquePtr client_ping_ptr,
+                                                   client_pong_ptr;
+
+
+    typename Client<bool>::UniquePtr client_terminate_ptr,
+                                client_flag_ptr;
+
+    Tensor<ScalarType, layout> data_read;
+
+    Tensor<bool> terminate;
+    Tensor<bool> flag;
+
+};
+
+// view API (this client uses the normal tensor API,
+// while the server will use the view API)
+template <typename P>
+class ViewsConsistencyChecks : public ::testing::Test {
+protected:
+
+    using ScalarType = typename P::type;
+    static const int layout = P::layout;
+
+    ViewsConsistencyChecks() :
+                   client_ping_ptr(new Client<ScalarType, layout>("SharsorIPCpp_view_ping",
+                                     name_space + getTypeAsString<ScalarType>() + std::to_string(layout),
+                                     false,
+                                     VLevel::V3)),
+                   client_pong_ptr(new Client<ScalarType, layout>("SharsorIPCpp_view_pong",
+                                     name_space + getTypeAsString<ScalarType>() + std::to_string(layout),
+                                     false,
+                                     VLevel::V3)),
+                   client_terminate_ptr(new Client<bool>("SharsorIPCpp_view_terminate",
+                                     name_space + getTypeAsString<ScalarType>() + std::to_string(layout),
+                                     false,
+                                     VLevel::V3)),
+                   client_flag_ptr(new Client<bool>("SharsorIPCpp_view_flag",
+                                     name_space + getTypeAsString<ScalarType>() + std::to_string(layout),
+                                     false,
+                                     VLevel::V3)){
+
+        // run all clients
+        client_ping_ptr->attach();
+        client_pong_ptr->attach();
+        client_terminate_ptr->attach();
+        client_flag_ptr->attach();
 
         getDataFromServer();
 
@@ -340,6 +494,7 @@ protected:
 
 };
 
+// Register the tests for normal tensor API
 TYPED_TEST_SUITE_P(ConsistencyChecks);
 
 TYPED_TEST_P(ConsistencyChecks, CheckConsistencyDTypes) {
@@ -350,11 +505,26 @@ TYPED_TEST_P(ConsistencyChecks, CheckConsistencyDTypes) {
 
 }
 
-// Register the tests
 REGISTER_TYPED_TEST_SUITE_P(ConsistencyChecks, CheckConsistencyDTypes);
 
-INSTANTIATE_TYPED_TEST_SUITE_P(My, ConsistencyChecks, MyTypes);
+INSTANTIATE_TYPED_TEST_SUITE_P(ConsistencyCheck, ConsistencyChecks, MyTypes);
 
+// Register the tests for view API
+TYPED_TEST_SUITE_P(ViewsConsistencyChecks);
+
+TYPED_TEST_P(ViewsConsistencyChecks, CheckConsistencyDTypes) {
+
+   check_comp_type(journal);
+
+   this->Run(); // run round-trip checks with client
+
+}
+
+REGISTER_TYPED_TEST_SUITE_P(ViewsConsistencyChecks, CheckConsistencyDTypes);
+
+INSTANTIATE_TYPED_TEST_SUITE_P(ConsistencyViewCheck, ViewsConsistencyChecks, MyTypes);
+
+// StringTensor checks
 TEST_F(StringTensorCheck, StringTensorChecks) {
 
     check_comp_type(journal);
--- SharsorIPCpp/tests/consistency_checks_srvr.cpp
+++ SharsorIPCpp/tests/consistency_checks_srvr.cpp
@@ -46,6 +46,7 @@ using MyTypes = ::testing::Types<
     TypeWithLayout<double, RowMajor>
 >;
 
+// normal tensor API
 template <typename P>
 class ConsistencyChecks : public ::testing::Test {
 protected:
@@ -169,6 +170,153 @@ protected:
                                 server_flag_ptr;
 
     Tensor<ScalarType, layout> data_read, data_written;
+
+    Tensor<bool> terminate = Tensor<bool>::Zero(1, 1); // flags to false
+    Tensor<bool> flag = Tensor<bool>::Zero(1, 1);
+
+    std::vector<bool> checks;
+
+};
+
+// view API (this server will use the view API for reading
+// and writing, the client side will use the normal tensor API)
+template <typename P>
+class ViewsConsistencyChecks : public ::testing::Test {
+protected:
+
+    using ScalarType = typename P::type;
+    static const int layout = P::layout;
+
+    ViewsConsistencyChecks() : rows(N_ROWS),
+                   cols(N_COLS),
+                   iterations(N_ITERATIONS),
+                   server_ping_ptr(new Server<ScalarType, layout>(rows, cols,
+                                     "SharsorIPCpp_view_ping",
+                                     name_space + getTypeAsString<ScalarType>() + std::to_string(layout),
+                                     false,
+                                     VLevel::V3,
+                                     true)),
+                   server_pong_ptr(new Server<ScalarType, layout>(rows, cols,
+                                     "SharsorIPCpp_view_pong",
+                                     name_space + getTypeAsString<ScalarType>() + std::to_string(layout),
+                                     false,
+                                     VLevel::V3,
+                                     true)),
+                   server_terminate_ptr(new Server<bool>(1, 1,
+                                     "SharsorIPCpp_view_terminate",
+                                     name_space + getTypeAsString<ScalarType>() + std::to_string(layout),
+                                     false,
+                                     VLevel::V3,
+                                     true)),
+                   server_flag_ptr(new Server<bool>(1, 1,
+                                     "SharsorIPCpp_view_flag",
+                                     name_space + getTypeAsString<ScalarType>() + std::to_string(layout),
+                                     false,
+                                     VLevel::V3,
+                                     true)),
+                   data_written(rows, cols),
+                   data_read(rows, cols),
+                   data_written_view(helpers::createViewFrom<ScalarType, layout>(
+                                         data_written,
+                                         0, 0, // indeces
+                                         rows, cols)),
+                   data_read_view(helpers::createViewFrom<ScalarType, layout>(
+                                      data_read,
+                                      0, 0, // indeces
+                                      rows, cols)){
+
+        // run all servers
+        server_ping_ptr->run();
+        server_pong_ptr->run();
+        server_terminate_ptr->run();
+        server_flag_ptr->run();
+
+        terminate(0, 0) = false;
+        server_terminate_ptr->writeTensor(terminate);
+
+    }
+
+    void Run() {
+
+        for (int i = 0; i < this->iterations; ++i) {
+
+            // here we write and read using views, to test the view API
+            // is working properly. The client side will use the normal
+            // tensor API to write and read
+
+            data_written.setRandom(); // randomize data
+
+            // writes down the whole random matrix on ping memory (view)
+            while (! server_ping_ptr->writeTensor(data_written_view, 0, 0)) {
+
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+
+            flag(0, 0) = true;
+            while(!server_flag_ptr->writeTensor(flag)) {
+                // try again
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+             // signals the client the write operation was completed
+
+            while (flag(0, 0)){
+
+                server_flag_ptr->readTensor(flag); // continue reading the flag
+
+                std::this_thread::sleep_for(std::chrono::microseconds(1)); // avoid busy wait loop
+
+            }
+            // client sets flag to false -> it has read the data and copied to the pong data
+
+            // reads data from pong memory (view)
+            while(!server_pong_ptr->readTensor(data_read_view, 0, 0)) {
+
+
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+
+            // being the views updated, also the original tensors should be
+            checks.push_back(areEqual<ScalarType>(data_written, data_read));
+
+        }
+
+        terminate(0, 0) = true;
+        while(!server_terminate_ptr->writeTensor(terminate)) {
+
+            // try again
+            std::this_thread::sleep_for(std::chrono::microseconds(1));
+
+        }
+
+    }
+
+    void SetUp() override {
+
+        // Initialization code (if needed)
+    }
+
+    void TearDown() override {
+
+        // close all servers
+        server_ping_ptr->close();
+        server_pong_ptr->close();
+        server_terminate_ptr->close();
+        server_flag_ptr->close();
+
+    }
+
+    int rows;
+    int cols;
+    int iterations;
+    typename Server<ScalarType, layout>::UniquePtr server_ping_ptr,
+                                                   server_pong_ptr;
+
+
+    typename Server<bool>::UniquePtr server_terminate_ptr,
+                                server_flag_ptr;
+
+    Tensor<ScalarType, layout> data_read, data_written;
+    TensorView<ScalarType, layout> data_read_view, data_written_view;
 
     Tensor<bool> terminate = Tensor<bool>::Zero(1, 1); // flags to false
     Tensor<bool> flag = Tensor<bool>::Zero(1, 1);
@@ -335,6 +483,7 @@ protected:
 
 };
 
+// Register the tests for normal tensor API
 TYPED_TEST_SUITE_P(ConsistencyChecks);
 
 TYPED_TEST_P(ConsistencyChecks, CheckConsistencyDTypes) {
@@ -354,11 +503,35 @@ TYPED_TEST_P(ConsistencyChecks, CheckConsistencyDTypes) {
 
 }
 
-// Register the tests
 REGISTER_TYPED_TEST_SUITE_P(ConsistencyChecks, CheckConsistencyDTypes);
 
-INSTANTIATE_TYPED_TEST_SUITE_P(My, ConsistencyChecks, MyTypes);
+INSTANTIATE_TYPED_TEST_SUITE_P(ConsistencyCheck, ConsistencyChecks, MyTypes);
 
+// Register the tests for view API
+TYPED_TEST_SUITE_P(ViewsConsistencyChecks);
+
+TYPED_TEST_P(ViewsConsistencyChecks, CheckConsistencyViewDTypes) {
+
+    check_comp_type(journal);
+
+    this->Run(); // run round-trip checks with client
+
+    int n_failures = 0;
+
+    bool success = allTrue(this->checks, n_failures);
+
+    std::cout << "Test completed. Number of failures: " << n_failures <<
+              "/" << N_ITERATIONS << std::endl;
+
+    ASSERT_TRUE(success);
+
+}
+
+REGISTER_TYPED_TEST_SUITE_P(ViewsConsistencyChecks, CheckConsistencyViewDTypes);
+
+INSTANTIATE_TYPED_TEST_SUITE_P(ConsistencyViewCheck, ViewsConsistencyChecks, MyTypes);
+
+// StringTensor checks
 TEST_F(StringTensorCheck, StringTensorChecks) {
 
     check_comp_type(journal);
